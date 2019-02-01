0e3156ec34c02dd7bb438396a65056f9c97442a8
한용구 <hyg1114@midasit.com>
2019-02-01 11:29:55 +0900

    레지스트리 갱신 관련 변수들을 환경설정 하였다. (lease-renewal-interval-in-seconds, lease-expiration-duration-in-seconds, eviction-interval-timer-in-ms)
    [Client] 인스턴스 목록을 출력하는 API 추가

--- client/src/main/java/com/msa/client/TestController.java
+++ client/src/main/java/com/msa/client/TestController.java
@@ -1,12 +1,37 @@
 package com.msa.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 public class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/ping")
+    public List<ServiceInstance> ping() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLIENT-SERVICE");
+        LOGGER.info("INSTANCES: count = {}", instances.size());
+        instances.forEach(it -> LOGGER.info("INSTANCE: id={}, prot={}", it.getServiceId(), it.getPort()));
+        return instances;
+    }
+
+
+
 }
